009459cf97f38a73a1e5d558397664b22b840d5e
VasuValeti <valeti.vasu@gmail.com>
2024-03-10 01:22:30 +0530

    test commit added comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//added by VasuBabu
 @SpringBootApplication
 public class DemoApplication {
 
